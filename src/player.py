2155937b9eda5cc5d5c9c7f41e0124c46535d8c5
danibaxx <danibaxx@hotmail.com>
2020-08-13 19:42:45 -0600

    working out how to have player carry items

--- src/player.py
+++ src/player.py
@@ -1,6 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-  def __init__(self, name, current_room=None):
-    self.name = name
+  def __init__(self, current_room=None):
     self.current_room = current_room
+
+class Backpack:
+  pass
+
+# add carry item class
